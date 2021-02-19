f404dd15bd9e9813719d75fe507b16e5938fa663
jnaing <jnaing@sfu.ca>
2021-02-19 01:27:13 -0800

    github link is working

--- src/components/Button.js
+++ src/components/Button.js
@@ -20,7 +20,7 @@ export const Button = ({
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
     return(
-        <Link to='/github' className='btn-mobile'>
+        <Link to='https://github.com/jsoeenaing' className='btn-mobile'>
             <button
             className={`btn ${checkButtonStyle} ${checkButtonSize}`}
             onClick={onClick}
