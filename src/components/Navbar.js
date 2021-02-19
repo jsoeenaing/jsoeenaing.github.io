b0b1078c8aa9436d8509e9719cf66ef7ba27a6f2
jnaing <jnaing@sfu.ca>
2021-02-19 01:19:58 -0800

    mobile fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,16 +10,6 @@ function Navbar() {
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
 
-    const showButton = () => {
-        if(window.innerWidth <= 960) {
-            setButton(false);
-        } else {
-            setButton(true);
-        }
-    };
-
-    window.addEventListener('resize', showButton);
-
     return (
         <>
         <nav className='navbar'>
