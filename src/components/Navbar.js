4ca0e47cd8824309e1214fd33a0265ea3c5330b3
jnaing <jnaing@sfu.ca>
2021-02-20 11:25:54 -0800

    links fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,17 +14,32 @@ function Navbar() {
         <>
         <nav className='navbar'>
             <div className='navbar-container'>
-                {button && <Button buttonStyle='btn--outline'>Github</
-                Button>}
+                <NavBarItem link={'https://github.com/jsoeenaing'}>
+                    Github
+                </NavBarItem>
                 &nbsp;&nbsp;&nbsp;&nbsp;
-                {button && <Button buttonStyle='btn--outline'>Resume</
-                Button>}
+                <NavBarItem link={'https://jsoeenaing.github.io/resume/'}>
+                    Resume
+                </NavBarItem>
                 &nbsp;&nbsp;&nbsp;&nbsp;
-                {button && <Button buttonStyle='btn--outline'>Linkedin</
-                Button>}
+                <NavBarItem link={'https://www.linkedin.com/in/jsoeenaing'}>
+                    Linkedin
+                </NavBarItem>
             </div>
         </nav>
         </>
+    );
+}
+
+function NavBarItem(props) {
+    return (
+        <div className={'navBarItemSpacing'}>
+            <a href={props.link}>
+            <Button buttonStyle='btn--outline'>
+                    {props.children}
+                </Button>
+            </a>
+        </div>
     );
 }
 
